04297098057614672642f51f04bdad171f3eafb3
alinazeng <alinazengziyun@yahoo.com>
2022-02-24 16:08:08 -0800

    modelling

--- Analyses/script_stan_glmer.R
+++ Analyses/script_stan_glmer.R
@@ -26,8 +26,8 @@ d <- read.csv("input/data_plot_Nov13_AllStudiesToDate.csv", header = TRUE)
 
 
 #1 ----
-fit1_lat <- stan_glmer(spring_event~lat_prov+ + (1|species)+ (1|garden_identifier), data = d)
-fit1_mat <- stan_glmer(spring_event~MAT_prov+ + (1|species)+ (1|garden_identifier), data = d)
+fit1_lat <- stan_glmer(spring_event~lat_prov + (1|species)+ (1|garden_identifier), data = d)
+fit1_mat <- stan_glmer(spring_event~MAT_prov + (1|species)+ (1|garden_identifier), data = d)
 
 #2 ----
 fit2_lat <- stan_glmer(spring_event~lat_prov*prov_continent + (1|species)+ (1|garden_identifier), data = d)
@@ -35,11 +35,10 @@ fit2_mat <- stan_glmer(spring_event~MAT_prov*prov_continent + (1|species)+ (1|ga
 
 # Here's an example of how to plot for latitude model ... can you repeat for MAT model?
 fit2_latsum <- summary(fit2_lat)
-fit2_latsum["(Intercept)", "mean"] # Intercept value for Europe 
+fit2_latsum["(Intercept)", "mean"] # Intercept value for Europe #112.9426
 
 # coef(fit2_lat)
-
-plot(spring_event~lat_prov, data=d, type="n")
+plot(spring_event~lat_prov, data=d, type="n", xlab = "Provenance latitude", ylab = "Spring DOY")
 points(spring_event~lat_prov, data=subset(d, prov_continent=="Europe"))
 points(spring_event~lat_prov, data=subset(d, prov_continent=="North America"), col="dodgerblue")
 
@@ -55,13 +54,50 @@ mcmc_areas(posterior,
            pars = c("lat_prov", "lat_prov:prov_continentNorth America"),
            prob = 0.8)
 
-# But we also want to look at the estimates for each species and garden -- these values are high, so likely explain much of the variation
+# But we also want to look at the estimates for each species and garden -- these values are high, so likely explain much of the variation #how?
 
-#2alt ___
-# Slow ... amd 279 divergent transitions, but Rhats look okay
+# MAT ----
+fit2_matsum <- summary(fit2_mat)
+fit2_matsum["(Intercept)", "mean"] # Intercept value for Europe  #125.4658
+
+# coef(fit2_mat)
+
+plot(spring_event~MAT_prov, data=d, type="n",xlab = "Provenance mean annual temperature (\u00B0C)", ylab = "Spring DOY")
+points(spring_event~MAT_prov, data=subset(d, prov_continent=="Europe"))
+points(spring_event~MAT_prov, data=subset(d, prov_continent=="North America"), col="dodgerblue")
+
+abline(fit2_matsum["(Intercept)", "mean"], fit2_matsum["MAT_prov", "mean"]) # line for Europe
+abline((fit2_matsum["(Intercept)", "mean"]+fit2_matsum["prov_continentNorth America", "mean"]),
+       (fit2_matsum["MAT_prov", "mean"] + fit2_matsum["MAT_prov:prov_continentNorth America", "mean"]), col="dodgerblue") # line for N America
+
+# We're initially most interested in these 'across garden and species' effects shown above ... so following...
+# https://mc-stan.org/bayesplot/
+posterior <- as.matrix(fit2_mat)
+
+mcmc_areas(posterior,
+           pars = c("MAT_prov", "MAT_prov:prov_continentNorth America"),
+           prob = 0.8)
+
+
+#2alt ----
+# Slow ... and 279 divergent transitions, but thats look okay #alina says: I had 1141 divergent transitions after warmup
 # fit2_latalt <- stan_glmer(spring_event~((lat_prov*prov_continent)|species)+ (1|garden_identifier), data = d)
-
-
+# Warning messages:
+# 1: There were 1141 divergent transitions after warmup. See
+# http://mc-stan.org/misc/warnings.html#divergent-transitions-after-warmup
+# to find out why this is a problem and how to eliminate them. 
+# 2: Examine the pairs() plot to diagnose sampling problems
+# 3: The largest R-hat is 1.6, indicating chains have not mixed.
+# Running the chains for more iterations may help. See
+# http://mc-stan.org/misc/warnings.html#r-hat 
+# 4: Bulk Effective Samples Size (ESS) is too low, indicating posterior means and medians may be unreliable.
+# Running the chains for more iterations may help. See
+# http://mc-stan.org/misc/warnings.html#bulk-ess 
+# 5: Tail Effective Samples Size (ESS) is too low, indicating posterior variances and tail quantiles may be unreliable.
+# Running the chains for more iterations may help. See
+# http://mc-stan.org/misc/warnings.html#tail-ess 
+# 6: Markov chains did not converge! Do not analyze results! 
+  
 #3 ---- 
 fit3_lat <- stan_glmer(spring_event~(lat_prov|species)+(1|garden_identifier), data = d)
 fit3_mat <- stan_glmer(spring_event~(MAT_prov|species)+(1|garden_identifier), data = d)

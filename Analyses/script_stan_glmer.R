3ca14eb0e5a5cc3826243d97c7852641139674ef
Elizabeth Wolkovich <wolkovich@biodiversity.edu>
2022-05-01 14:08:20 -0700

    Adding some notes, running some models and thinking on next steps ...

--- Analyses/script_stan_glmer.R
+++ Analyses/script_stan_glmer.R
@@ -65,6 +65,45 @@ fit1_mat <- stan_glmer(spring_event~MAT_prov + (1|species)+ (1|garden_identifier
 fit2_lat <- stan_glmer(spring_event~lat_prov*prov_continent + (1|species)+ (1|garden_identifier), data = d)
 fit2_mat <- stan_glmer(spring_event~MAT_prov*prov_continent + (1|species)+ (1|garden_identifier), data = d)
 
+
+### New bits by Lizzie on 1 May 2022
+fit1 <- stan_glmer(spring_event~lat_prov*prov_continent + (1|species), data = d)
+fit2 <- stan_glmer(spring_event~(lat_prov|species)*prov_continent, data = d)
+fit3 <- stan_glmer(spring_event~lat_prov*prov_continent + (1|species) + (1|garden_identifier), data = d)
+fit4 <- stan_glmer(spring_event~(lat_prov|species)*prov_continent + (1|garden_identifier), data = d)
+fit5 <- stan_glmer(spring_event~(lat_prov|species), data = d) 
+fit6 <- stan_glmer(spring_event~(lat_prov|species) + (1|garden_identifier), data = d) # if you want to do loo, I would compare models like fit5 to models like this, which includes a different intercept for each garden as well
+
+
+# Understanding the output  ...
+summary(fit1)
+# 'Model info' is just that, it tells up what model we fit and how many observations, groups etc... You'll see here that it uses 'stan_lmer.' You should always give this a quick glance to make sure it sounds right to you.   
+# Below are the estimates, these are what we need to plot, understand and report to our audiences (people at talks, eventually the readers and reviewers of papers)
+# (Intercept) is just that -- so for this model the intercept is 90.9 day of year.
+# lat_prov  is the effect of latitude of provenance (in degrees of latitude). So for every 1 degree north the day of year gets 0.7 days later ... but since we have an effect of continent this estimate is for EUROPE. This is a small effect but the uncertainty interavals for 2.5 to 97.5 are 0.2 to 1.1, meaning there is likely a small effect
+# prov_continentNorth America  gives us the effect for NA, it says for every 1 degree north the day of year gets 15.7 days later
+# Next, it gives us the species offsets, so Alnus rubra has an intercept of (Intercept) + -28.4 roughly and so on ... rstanarm will actually calculate these for you. Check out ...
+coef(fit1)
+# sigma is the 'measurement error' meaning that after accounting for everything else in our model there is about 8.2 days of 'natural' or 'unexplained' variation
+
+# These estimates represent mean, 2.5% etc. estimates from the posteriors and you can use the posteriors themselves to further summarize the models, which I think we may want to do. 
+
+# For fit2, you get different intercepts and slopes for each species, based on the new plots you've made this looks like what your data needs to me (there is not ONE slope across all the data that I see) 
+
+# For fit3, you can see many of the garden_identifier effects are small but some like for Q and R are large; you also get a sigma associated with garden and species which tells us the overall variation in each... so Sigma[garden_identifier:(Intercept),(Intercept)] is about 2.2 (this is a sigma value so does NOT mean +2.2 days, it's better to think of these as relative to one another) and the species is 3.9, suggesting species explains about 2X variation as the gardens. 
+
+# For plotting, if we wanted for something like fit1 (random intercepts only):
+# each species has a unique intercept that you can extract from coef(fit1) and the slopes are the same so you'd plot the overall slope of 0.7 for all species (we sort of want to plot the European slope (0.7 here) for European species and the NA one for NA species, but the model is not set up that way). We'd want one line per species, color-coded to match the species dots. (We could also plot the overall effects as thicker lines, but I think it might be better to show those effects as we have been the posterior density plots.)
+# And for fit2 you'd have a unique slope and intercept to plot for each species, which you can get in:
+coef(fit2)
+
+# comparing some output ...
+plot(coef(fit5)$species["lat_prov"][,]~coef(fit4)$species["lat_prov"][,]) # species estimates are relatively similar with or without continent
+
+
+### End of new bits by Lizzie on 1 May 2022
+
+
 # Here's an example of how to plot for latitude model ... can you repeat for MAT model?
 fit2_latsum <- summary(fit2_lat)
 fit2_latsum["(Intercept)", "mean"] # Intercept value for Europe #112.9426
